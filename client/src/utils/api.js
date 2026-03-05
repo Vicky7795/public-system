fe536f74e0ce16e982082189f10390e58c038c45
Sourabh Nagare <sourabhdn8@gmail.com>
2026-03-05 16:06:08 +0530

    Implement Vite proxy for robust multi-device connectivity

--- client/src/utils/api.js
+++ client/src/utils/api.js
@@ -1,13 +1,7 @@
 import axios from 'axios';
 
-const getBaseURL = () => {
-    if (import.meta.env.VITE_API_BASE_URL) return import.meta.env.VITE_API_BASE_URL;
-    const { hostname } = window.location;
-    return `http://${hostname}:5000/api`;
-};
-
 const api = axios.create({
-    baseURL: getBaseURL(),
+    baseURL: '/api',
 });
 
 api.interceptors.request.use((config) => {
