d4c20bb9a7d6f29d4eb1d64d5517a8f301b72ef9
Sourabh Nagare <sourabhdn8@gmail.com>
2026-04-13 20:12:10 +0530

    Final cleanup and deployment stabilization

--- server/index.js
+++ server/index.js
@@ -1,6 +1,5 @@
 const express = require('express');
 const mongoose = require('mongoose');
-const cors = require('cors');
 const path = require('path');
 require('dotenv').config({ path: path.join(__dirname, '.env') });
 
